5cfe38f49b3523e1a3cc1c2637f783b6cb9b0eb4
Robin Jun <robinjun@Robins-MacBook-Pro-2.local>
2020-07-13 03:00:24 -0500

    added auth service and login methods

--- DevLibraryApp-SPA/src/app/login/login.component.ts
+++ DevLibraryApp-SPA/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,13 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   login() {
     console.log('login submitted');
+    this.authService.login(this.model).subscribe(next => {
+      console.log('logged in successfully');
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
