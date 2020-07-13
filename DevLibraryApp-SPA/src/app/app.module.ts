28456d562e0f73f8c0bd1989009749ba52331560
Robin Jun <robinjun@Robins-MacBook-Pro-2.local>
2020-07-13 02:36:03 -0500

    new login component with basic form

--- DevLibraryApp-SPA/src/app/app.module.ts
+++ DevLibraryApp-SPA/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PagesComponent } from './pages/pages.component';
 import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       PagesComponent,
-      NavComponent
+      NavComponent,
+      HomeComponent,
+      LoginComponent
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
    ],
    providers: [],
    bootstrap: [
