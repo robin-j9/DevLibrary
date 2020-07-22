50f79d03833239df5b002bcf79c43d6d9693e570
Robin Jun <robinjun@Robins-MacBook-Pro-2.local>
2020-07-22 02:29:08 -0500

    routing setup with auth guard

--- DevLibraryApp-SPA/src/app/routes.ts
+++ DevLibraryApp-SPA/src/app/routes.ts
@@ -5,13 +5,21 @@ import { CategoriesComponent } from './categories/categories.component';
 import { TagsComponent } from './tags/tags.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'pages', component: PagesComponent },
-  { path: 'categories', component: CategoriesComponent },
-  { path: 'tags', component: TagsComponent },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'pages', component: PagesComponent },
+      { path: 'categories', component: CategoriesComponent },
+      { path: 'tags', component: TagsComponent },
+    ],
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
